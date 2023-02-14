f1262ddf0d22e3cabb2ef2f68959a55488d843a4
GeralmyUrena <geralmyuri11@gmail.com>
2023-02-13 21:40:13 -0500

    passed

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div>
       <NavBar />
+      
       <Home />
       <About />
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import { name as Name, city as City} from "../data/data.js";
+
+const divStyle = {
+  color: 'firebrick'}
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  
+  return (<div id="home">Home
+   <h1 style={divStyle}> {Name} is a Web Developer from {City}</h1>
+  
+  </div>)
+  
 }
 
 export default Home;
